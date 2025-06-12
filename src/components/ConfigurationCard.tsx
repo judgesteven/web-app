c6c1865b6c38941af0dcd94612db121f7edf4995
Steven Judge <stevenjudge@Stevens-MacBook-Pro.local>
2025-06-12 11:46:16 +0300

    fix: Properly set up heroicons v1 with type declarations

--- src/components/ConfigurationCard.tsx
+++ src/components/ConfigurationCard.tsx
@@ -8,19 +8,7 @@ import AchievementsCard from './AchievementsCard'
 import MysteryCard from './MysteryCard'
 import LeaderboardCard from './LeaderboardCard'
 import { toast } from 'react-hot-toast'
-
-// Custom SVG Icons
-const ChevronUpIcon = ({ className = "w-5 h-5" }: { className?: string }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" className={className} viewBox="0 0 20 20" fill="currentColor">
-    <path fillRule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clipRule="evenodd" />
-  </svg>
-)
-
-const ChevronDownIcon = ({ className = "w-5 h-5" }: { className?: string }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" className={className} viewBox="0 0 20 20" fill="currentColor">
-    <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
-  </svg>
-)
+import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/react/outline'
 
 interface Mission {
   id: string
--- src/types/heroicons.d.ts
+++ src/types/heroicons.d.ts
@@ -1,2 +1,16 @@
-declare module '@heroicons/react/outline';
+import { FC, SVGProps } from 'react'
+
+export interface IconProps extends SVGProps<SVGSVGElement> {
+  title?: string
+  titleId?: string
+}
+
+declare module '@heroicons/react/outline' {
+  export const ChevronUpIcon: FC<IconProps>
+  export const ChevronDownIcon: FC<IconProps>
+  export const CheckIcon: FC<IconProps>
+  export const ChevronDoubleUpIcon: FC<IconProps>
+  export const ChevronDoubleDownIcon: FC<IconProps>
+}
+
 declare module '@heroicons/react'; 
